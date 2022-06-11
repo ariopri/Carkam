a15b0e9c1e53cff26b9844e5569fbe8e43b85002
nurfaidzy <nurfaidzy@gmail.com>
2022-06-11 09:11:08 +0700

    Home Page Done

--- front_end/src/components/BerandaItem/Berita.jsx
+++ front_end/src/components/BerandaItem/Berita.jsx
@@ -1,7 +1,8 @@
+import GBerita from "./image/berita.jpg";
 const Berita = () => {
   return (
     <>
-      <div className="container spaciberita">
+      <div className="container spaciberita spacekebawahbrita">
         <center>
           <h2>Berita terakhir</h2>
 
@@ -9,33 +10,42 @@ const Berita = () => {
             <div className="col-md-4">
               <div className="card border-0">
                 <div className="card-body">
-                  <p className="card-text">
+                  <img src={GBerita} alt="" width="300px" />
+                  <div className="spaciberita">
+                    <p className="card-text">
                     Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
                     justo odio, dapibus ac facilisis in, egestas eget quam.
                     Donec id elit non mi porta gravida at eget metus.
                   </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="col-md-4">
               <div className="card border-0">
                 <div className="card-body">
-                  <p className="card-text">
+                <img src={GBerita} alt="" width="300px" />
+                  <div className="spaciberita">
+                    <p className="card-text">
                     Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
                     justo odio, dapibus ac facilisis in, egestas eget quam.
                     Donec id elit non mi porta gravida at eget metus.
                   </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="col-md-4">
               <div className="card border-0">
                 <div className="card-body">
-                  <p className="card-text">
+                <img src={GBerita} alt="" width="300px" />
+                  <div className="spaciberita">
+                    <p className="card-text">
                     Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
                     justo odio, dapibus ac facilisis in, egestas eget quam.
                     Donec id elit non mi porta gravida at eget metus.
                   </p>
+                  </div>
                 </div>
               </div>
             </div>
---
+++ front_end/src/components/BerandaItem/Footer.jsx
@@ -0,0 +1,39 @@
+const Footer = () => {
+  return (
+    <>
+      <div class="mt-5 pt-5 pb-5 footer bg-dark text-light">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5 col-xs-12 ">
+              <h2>C A R K A M . I D</h2>
+              <p class="pr-5 text-white-50">
+                Jelajahi informasi kampus - kampus di Indonesia berdasarkan
+                lokasi, biaya kuliah, jurusan serta passing grade untuk
+                membantumu memilih kampus terbaik.{" "}
+              </p>
+            </div>
+            <div class="col-lg-3 col-xs-12 ">
+              <h4 class="mt-lg-0 mt-sm-3">Email</h4>
+              <p class="pr-5 text-white-50">carkam.id@gmail.com </p>
+            </div>
+            <div class="col-lg-4 col-xs-12 ">
+              <h4 class="mt-lg-0 mt-sm-4">Telephone</h4>
+              <p class="pr-5 text-white-50">+62 21 4873 392 </p>
+            </div>
+          </div>
+          <div class="row mt-5">
+            <div class="col copyright">
+              <p class="">
+                <small class="text-white-50">
+                  <center>© 2019. All Rights Reserved.</center>
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
