1e5b18465679a3e98c1d2d342f4de360d6558f33
nurfaidzy <nurfaidzy@gmail.com>
2022-06-15 22:33:30 +0700

    Page PAQ -> Done

---
+++ front_end/src/components/FAQ.jsx
@@ -0,0 +1,15 @@
+import Isifaq from "./FAQItem/Isifaq";
+import Jumbo from "./FAQItem/Jumbo";
+import Pesan from "./FAQItem/Pesan";
+
+const FAQ = () => {
+  return (
+    <>
+      <Jumbo />
+      <Isifaq />
+      <Pesan />
+    </>
+  );
+};
+
+export default FAQ;
---
+++ front_end/src/components/FAQItem/Jumbo.jsx
@@ -0,0 +1,11 @@
+import Back1 from "./image/FAQ-Carkam.png";
+
+const Jumbo = () => {
+  return (
+    <>
+      <img src={Back1} alt="FAQ" width="100%" />
+    </>
+  );
+};
+
+export default Jumbo;
