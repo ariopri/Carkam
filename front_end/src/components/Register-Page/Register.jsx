50e79456694175d2ccad3a2162bc1ac435a4fc37
nurfaidzy <nurfaidzy@gmail.com>
2022-06-16 21:08:46 +0700

    Memperbaiki Register Page -> Done

--- front_end/src/components/Register-Page/Register.jsx
+++ front_end/src/components/Register-Page/Register.jsx
@@ -1,117 +1,61 @@
-import React, { useState } from 'react';
-import './Register.scss';
-import { Link } from 'react-router-dom';
-import logo from './image/logo-2.png';
-import registerImg from './image/register.png';
-
-function Register() {
-  const [email, setemail] = useState('');
-  const [Fusername, setFusername] = useState('');
-  const [Susername, setSusername] = useState('');
-  const [pwd1, setpwd1] = useState('');
-  const [pwd2, setpwd2] = useState('');
-
-  const handlesubmit = (e) => {
-    e.preventDefault();
-  };
-
+import { Container } from "react-bootstrap";
+import Logo from "./image/logo.png";
+import Gambaran from "./image/login.png";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+const Register = () => {
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <div className="main-Register">
-            <div className="left-side">
-              <div className="header">
-                <img src={logo} id="logo-img" alt="" srcset="" />
+      <div className="bg-dark text-light ">
+        <div className="pt-3">
+          <Container>
+            <div class="row">
+              <div class="col-md-6">
+                <center>
+                  <img src={Logo} alt="logo" className="img-fluid" />
+                  <div>
+                    <img src={Gambaran} alt="" />
+                  </div>
+                </center>
               </div>
-              <div className="body">
-                <img src={registerImg} id="reg-img" alt="" srcset="" />
-              </div>
-              <p>“You can teach a student a lesson for a day; but if you can teach him to learn by creating curiosity, he will continue the learning process as long as he lives.”</p>
-            </div>
-            <div className="right-side">
-              <div className="top-right">
-                <p>
-                  Already have an Account?
-                  <Link id="Links-signin" to="/Login">
-                    Sign In
-                  </Link>
-                </p>
-              </div>
-              <div className="body-right">
-                <div className="container">
-                  <h1>Create Account!</h1>
-                  <form onSubmit={handlesubmit}>
-                    <div className="input-group">
-                      <h5>First Name</h5>
-                      <input
-                        type="text"
-                        name="Fname"
-                        value={Fusername}
-                        onChange={(e) => {
-                          setFusername(e.target.value);
-                        }}
-                        id="fname"
-                      />
-                    </div>
-                    <div className="input-group">
-                      <h5>Last Name</h5>
-                      <input
-                        type="text"
-                        name="lname"
-                        value={Susername}
-                        onChange={(e) => {
-                          setSusername(e.target.value);
-                        }}
-                        id="lname"
-                      />
-                    </div>
-                    <div className="input-group">
-                      <h5>Email</h5>
-                      <input
-                        type="Email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => {
-                          setemail(e.target.value);
-                        }}
-                        id="email1"
-                      />
-                    </div>
-                    <div className="input-group">
-                      <h5>Password</h5>
-                      <input
-                        type="password"
-                        name="pwd"
-                        value={pwd1}
-                        onChange={(e) => {
-                          setpwd1(e.target.value);
-                        }}
-                        id="pwd1"
-                      />
-                    </div>
-                    <div className="input-group">
-                      <h5>Confirm Password</h5>
-                      <input
-                        type="password"
-                        name="pwd"
-                        value={pwd2}
-                        onChange={(e) => {
-                          setpwd2(e.target.value);
-                        }}
-                        id="pwd2"
-                      />
-                    </div>
-                    <input type="submit" id="sbtn" value="SUBMIT" />
-                  </form>
+              <div class="col-md-6">
+                <div className="p-4">
+                  <div className="bg-light p-4 text-dark">
+                    <center>
+                      <h3>
+                        <b>Create Account</b>
+                      </h3>
+                    </center>
+                    <Form className="pt-3">
+                      <Form.Group className="mb-3">
+                        <Form.Control type="text" placeholder="User Name" />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Control type="email" placeholder="Email" />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <Button variant="dark w-100" type="submit">
+                        Submit
+                      </Button>
+                    </Form>
+                    <center>
+                      <div className="p-3 text-muted ">
+                        <a href="/Login" className="text-reset">
+                          Sudah punya akun mari kesini
+                        </a>
+                      </div>
+                    </center>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </header>
+          </Container>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default Register;
