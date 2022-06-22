9e101430fbd06f56c78f110b85f679699851d89a
nurfaidzy <nurfaidzy@gmail.com>
2022-06-22 21:10:45 +0700

    API LIST

--- front_end/src/components/KampusItem/ListKampus.jsx
+++ front_end/src/components/KampusItem/ListKampus.jsx
@@ -1,204 +1,66 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import KM from "./image/KM.png";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const ListKampus = () => {
+  const baseUrl = "https://reqres.in";
+  const token = localStorage.getItem("token");
+
+  const [data, setdata] = useState([]);
+
+  const getdata = async () => {
+    const res = await axios.get(`${baseUrl}/api/users`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    setdata(res.data.data);
+  };
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
-    <div>
-      <center>
-        <h3>Pilih sepuasnya dan bedah hingga dapat</h3>
-      </center>
-      <div class="container">
-        <div class="row pt-3">
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-        <div class="row pt-3">
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-          <div class="col-md-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={KM} />
-              <Card.Body>
-                <Card.Title>Kampus Merdeka</Card.Title>
-                <Card.Text>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Ullam, praesentium nam dignissimos repellendus voluptates
-                    tempore.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Terdapat 7 Jurusan</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="/Kampus/detail" className="btn btn-success">
-                  Lihat
-                </Card.Link>
-              </Card.Body>
-            </Card>
+    <>
+      <div>
+        <center>
+          <h3>Pilih sepuasnya dan bedah hingga dapat</h3>
+        </center>
+        <div class="container">
+          <div class="row pt-3">
+            {data.map((item) => (
+              <div class="col-md-3 ">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={KM} />
+                  <Card.Body>
+                    <Card.Title>{item.first_name}</Card.Title>
+                    <Card.Text>
+                      <p>
+                        {item.first_name} {item.last_name}
+                      </p>
+                    </Card.Text>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>{item.email}</ListGroup.Item>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link
+                      href="/Kampus/detail"
+                      className="btn btn-success"
+                    >
+                      Lihat
+                    </Card.Link>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default ListKampus;
