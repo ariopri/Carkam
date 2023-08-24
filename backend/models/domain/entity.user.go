77f236fa948a153bcd8ff2077953fca8929119c0
arioprima <jayaarya413@gmail.com>
2023-08-24 23:03:46 +0700

    update: change the directory position of an entity file

--- backend/models/domain/entity.user.go
+++ backend/models/domain/entity.user.go
@@ -2,17 +2,17 @@ package models
 
 import "time"
 
-type EntityStudent struct {
+type EntityUser struct {
 	ID        string
 	FirstName string
 	LastName  string
 	UserName  string
-	Phone     string
 	Email     string
+	Phone     *string
 	Password  string
-	Role      string
-	Kota      string
-	Avatar    string
+	Role      *string
+	Kota      *string
+	Avatar    *string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
