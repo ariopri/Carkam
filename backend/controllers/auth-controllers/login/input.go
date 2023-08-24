fd239cbc03af2753ac012cfe25cc3698f25a5a7c
arioprima <jayaarya413@gmail.com>
2023-08-24 23:28:11 +0700

    delete: deletes files and directories in the controllers folder

--- backend/controllers/auth-controllers/login/input.go
+++
@@ -1,6 +0,0 @@
-package loginAuth
-
-type InputLogin struct {
-	UserName string `json:"username" validate:"required,min=1,max=50,lowercase"`
-	Password string `json:"password" validate:"required,min=1,max=50"`
-}
