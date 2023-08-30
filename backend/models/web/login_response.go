35993c5d98c451e36130835b2126100f19660b59
arioprima <jayaarya413@gmail.com>
2023-08-30 20:15:36 +0700

    update: update login response

--- backend/models/web/login_response.go
+++ backend/models/web/login_response.go
@@ -1,6 +1,8 @@
 package web
 
 type LoginResponse struct {
+	ID        string `json:"id"`
+	UserName  string `json:"username"`
 	TokenType string `json:"token_type"`
 	Token     string `json:"token"`
 	Role      string `json:"role"`
