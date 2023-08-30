8683f7a293c47a20f13712d62eebf7cee278a7cb
arioprima <jayaarya413@gmail.com>
2023-08-30 21:03:13 +0700

    update: update main file

--- backend/go.mod
+++ backend/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/go-playground/validator/v10"
+	_ "github.com/lib/pq"
+	"github.com/rg-km/final-project-engineering-66/config"
+	"github.com/rg-km/final-project-engineering-66/controllers"
+	"github.com/rg-km/final-project-engineering-66/repository"
+	"github.com/rg-km/final-project-engineering-66/route"
+	"github.com/rg-km/final-project-engineering-66/service"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, map[string]string{
-			"message": "pong",
-		})
-	})
-	err := r.Run(":8080")
+	loadConfig, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+	db, err := config.ConnectionDB(&loadConfig)
+	if err != nil {
+		panic(err)
+	}
+	validate := validator.New()
+
+	userRepository := repository.NewUsersRepository(db)
+	userService := service.NewUserServiceImpl(userRepository, db, validate)
+	userController := controllers.NewUserController(userService)
+
+	router := route.NewRouter(userRepository, userController)
+
+	err = router.Run(":8080")
 	if err != nil {
 		return
 	}
